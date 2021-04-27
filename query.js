6a1f3f528600f4b3422f02e6a417a989194288ae
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-04-27 18:58:31 -0400

    third commit

---
+++ query.js
@@ -0,0 +1,5 @@
+const db = require("./db");
+const Brand = require("./models/brand");
+const Product = require("./models/product");
+
+
--- seed/brandsProducts.js
+++ seed/brandsProducts.js
@@ -33,15 +33,52 @@ const main = async () => {
 
   const products = [
     {
-      title: "AirPods",
+      title: "Apple AirPods",
       description: "https://www.apple.com/airpods",
       price: "250",
       brand: brand1._id,
     },
+    {
+      title: "Apple iPhone Pro",
+      description: "https://www.apple.com/iphone-11-pro",
+      price: "1000",
+      brand: brand1._id,
+    },
+    {
+      title: "Apple Watch",
+      description: "https://www.apple.com/watch",
+      price: "499",
+      brand: brand1._id,
+    },
+    {
+      title: "Vespa Primavera",
+      description: "https://www.vespa.com/us_EN/vespa-models/primavera.html",
+      price: "3000",
+      brand: brand2._id,
+    },
+    {
+      title: "New Balance 574 Core",
+      description: "https://www.newbalance.com/pd/574-core/ML574-EG.html",
+      price: "84",
+      brand: brand3._id,
+    },
+    {
+      title: "Tribe Messenger Bike 004",
+      description:
+        "https://tribebicycles.com/collections/messenger-series/products/mess-004-tx",
+      price: "675",
+      brand: brand4._id,
+    },
+    {
+      title: "Stumptown Hair Bender Coffee",
+      description: "https://www.stumptowncoffee.com/products/hair-bender",
+      price: "17",
+      brand: brand5._id,
+    },
   ];
 
-  await Book.insertMany(books);
-  console.log("Books Created with Publisher!");
+  await Product.insertMany(products);
+  console.log("Many products created with brand!");
 };
 
 const run = async () => {
