4c34a65d1326a6813d2cdf43413cc9c728004b94
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-04-27 20:15:10 -0400

    eigth commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = require("./db");
+const Brand = require("./models/brand");
 const Product = require("./models/product");
 
 db.on("error", console.error.bind(console, "MongoDB Connection Error!"));
@@ -30,7 +31,26 @@ app.get("/products/:id", async (req, res) => {
     res.json(product);
   } catch (e) {
     console.log(e);
-    res.send()
+    res.send();
+  }
+});
+
+app.get("/brands", async (req, res) => {
+  const brands = await Brand.find();
+  res.json(brands);
+});
+
+app.get("/brands/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const brand = await Brand.findById(id);
+    if (!brand) {
+      throw Error("Could not find brand.");
+    }
+    res.json(brand);
+  } catch (e) {
+    console.log(e);
+    res.send();
   }
 });
 
